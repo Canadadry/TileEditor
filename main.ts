f222116446c1ff18aaae516a9247cc212a5d1eb6
Mourey jerome <jerome@everycheck.fr>
2020-08-09 19:44:23 +0200

    re-arrange

--- main.ts
+++ main.ts
@@ -1,15 +1,11 @@
-import {Position,FixedPosition,LayoutPosition,XAlign,YAlign} from './src/position';
-import {Size} from './src/size';
-import {Window} from './src/window';
-import {Layout,ColumnLayout,RowLayout,NoChildLayout,GridLayout} from './src/layout';
-import {Frame,PaintFunction,} from './src/frame'
+import {Position,FixedPosition,LayoutPosition,XAlign,YAlign} from './src/frame/position';
+import {Size} from './src/frame/size';
+import {Window} from './src/frame/window';
+import {Layout,ColumnLayout,RowLayout,NoChildLayout,GridLayout} from './src/frame/layout';
+import {Frame,PaintFunction,} from './src/frame/frame';
+import {Rectangle} from './src/painter/rectangle';
+import {Colors} from './src/painter/color';
 
-function rectangle(r:number,g:number,b:number) :PaintFunction{
-	return (x:number,y:number,width:number,height:number)=>{
-		love.graphics.setColor(r,g,g,255)
-		love.graphics.rectangle("fill",x,y,width,height)
-	}
-}
 
 let window:Window;
 let frame:Frame;
@@ -31,16 +27,16 @@ love.load = function() {
 		new Size(200,600),
 		window,
 		new GridLayout(3,10),
-		rectangle(1,1,1)
+		Rectangle(Colors.White)
 	)
 
 	for(let i=0;i<7;i++){
 		let subframe = new Frame(
 			new LayoutPosition(),
-			new Size(100,100),
+			new Size(200,100),
 			frame,
 			new NoChildLayout(),
-			rectangle(1,0,0)
+			Rectangle(Colors.Red,50)
 		)
 	}
 }
@@ -49,7 +45,7 @@ love.mousepressed = function( x:number, y:number, button:number,isTouch:boolean 
 	picked = frame.pick(x,y);
 	if(picked){
 		lastPaint = picked.paint
-		picked.paint = rectangle(0,1,1)
+		picked.paint = Rectangle(Colors.Blue)
 	}
 }
 
