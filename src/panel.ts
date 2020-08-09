c3bde571a962da3d21e0f6196690b82173e5f5e4
Mourey jerome <jerome@everycheck.fr>
2020-08-09 12:34:20 +0200

    small refacto

--- src/panel.ts
+++ src/panel.ts
@@ -30,13 +30,14 @@ export class Panel{
 	opaque:boolean;
 	needsLayout:boolean;
 	children:Array<Panel>;
-	parent:Panel;
+	parent:Panel|null;
 	align:Align;
 	border:number;
 	inset:number;
-	bg:Panel;
+	bg:Panel|null;
 	parentW:number;
 	parentH:number;
+	layout:(()=>void)|null;
 
 	constructor(params:PanelParam){
 
@@ -68,115 +69,110 @@ export class Panel{
 	}
 
 	draw(){
-		if (!this.visible){return}    
-
-			if (this.bg){
-				this.bg.fill = this.fill
-			}
-			love.graphics.push("all")
-			love.graphics.translate(this.frame.x, this.frame.y)
-
-			for(let i=0;i<this.children.length;i++){
-				this.children[i].draw()
-			}
-			love.graphics.pop()
+		if (!this.visible){
+			return
 		}
 
-		update(){
-			let pw:number = love.window.getMode()[0]
-			let ph:number = love.window.getMode()[1]
+		if (this.bg){
+			this.bg.fill = this.fill
+		}
+		love.graphics.push("all")
+		love.graphics.translate(this.frame.x, this.frame.y)
 
-			if(this.parent){
-				pw = this.parent.frame.w
-				ph = this.parent.frame.h
-			}
+		for(let i=0;i<this.children.length;i++){
+			this.children[i].draw()
+		}
+		love.graphics.pop()
+	}
 
-			if (pw != this.parentW){
-				this.ajustToParentWidth(pw)
-			}
-			if (ph != this.parentH){
-				this.ajustToParentwHeight(ph)
-			}
-
-			if (this.parent ){
-				this.frame.w = this.parent.frame.w * this.size.x 
-				this.frame.h = this.parent.frame.h * this.size.y
-				this.frame.x = this.anchor.x * this.parent.frame.w - this.frame.w * this.pivot.x
-				this.frame.y = this.anchor.y * this.parent.frame.h - this.frame.h * this.pivot.y
-
-				this.tframe.x = this.parent.tframe.x + this.frame.x
-				this.tframe.y = this.parent.tframe.y + this.frame.y
-			}else{
-				this.frame.w = love.window.getMode()[0] * this.size.x
-				this.frame.h = love.window.getMode()[1] * this.size.y
-				this.frame.x = this.anchor.x * love.window.getMode()[0] - this.frame.w * this.pivot.x
-				this.frame.y = this.anchor.y * love.window.getMode()[1] - this.frame.h * this.pivot.y
-
-				this.tframe.x = this.frame.x
-				this.tframe.y = this.frame.y
-			} 
-
-			this.tframe.w = this.frame.w
-			this.tframe.h = this.frame.h 
-
-			if (this.needsLayout){// && this.layout) { 
-				// this.layout() 
-				this.needsLayout = false
-			}
-
-			for(let i=0;i<this.children.length;i++){
-				this.children[i].update()
-			}
+	update(){
+		let fakeParent = false;
+		if(!this.parent){
+			this.parent = new Panel(new PanelParam());
+			this.parent.frame.w = love.window.getMode()[0]
+			this.parent.frame.h = love.window.getMode()[1]
+			this.parent.tframe.x = 0
+			this.parent.tframe.y = 0
+			fakeParent = true;
 		}
 
-		ajustToParentWidth(pw:number){
-			let w = this.size.x * this.parentW;
-			let x = this.anchor.x * this.parentW - this.pivot.x * w
+		let pw:number = this.parent.frame.w
+		let ph:number = this.parent.frame.h
 
-			if( this.align.h == "LEFT")
-			{
-				x = x + this.pivot.x * w
-				this.anchor.x = x / pw
-			}else if(this.align.h == "CENTER"){
-				x = x - this.parentW/2 + pw/2 + this.pivot.x * w
-				this.anchor.x = x / pw
-			}else if(this.align.h == "RIGHT" ){
-				x = x - this.parentW + pw + this.pivot.x * w
-				this.anchor.x = x / pw
-			}else if(this.align.h == "STRETCH"){
-				let left = x
-				let right = x - this.parentW + pw + w
-				w = right - left
-				this.anchor.x = (left + this.pivot.x * w) / pw
-			}
-
-			this.size.x = w / pw
-			this.parentW = pw
+		if (pw != this.parentW){
+			this.ajustToParentWidth(pw)
+		}
+		if (ph != this.parentH){
+			this.ajustToParentwHeight(ph)
 		}
 
-		ajustToParentwHeight(ph:number){
-			let h = this.size.y * this.parentH
-			let y = this.anchor.y * this.parentH - this.pivot.y * h   
+		this.frame.w = this.parent.frame.w * this.size.x 
+		this.frame.h = this.parent.frame.h * this.size.y
+		this.frame.x = this.anchor.x * this.parent.frame.w - this.frame.w * this.pivot.x
+		this.frame.y = this.anchor.y * this.parent.frame.h - this.frame.h * this.pivot.y
 
-			if (this.align.v == "BOTTOM" ){
-				y = y + this.pivot.y * h
-				this.anchor.y = y / ph
-			}else if( this.align.v == "CENTER" ){
-				y = y - this.parentH/2 + ph/2 + this.pivot.y * h
-				this.anchor.y = y / ph
-			}else if( this.align.v == "TOP" ){
-				y = y - this.parentH + ph + this.pivot.y * h
-				this.anchor.y = y / ph
-			}else if( this.align.v == "STRETCH" ){
-				let left = y
-				let right = y - this.parentH + ph + h
-				h = right - left
-				this.anchor.y = (left + this.pivot.y * h) / ph
-			}
+		this.tframe.w = this.frame.w
+		this.tframe.h = this.frame.h 
+		this.tframe.x = this.parent.tframe.x + this.frame.x
+		this.tframe.y = this.parent.tframe.y + this.frame.y
 
-			this.size.y = h / ph
-			this.parentH = ph
+		if (this.needsLayout &&  this.layout) { 
+			this.layout() 
+			this.needsLayout = false
 		}
+
+		for(let i=0;i<this.children.length;i++){
+			this.children[i].update()
+		}
+	}
+
+	ajustToParentWidth(pw:number){
+		let w = this.size.x * this.parentW;
+		let x = this.anchor.x * this.parentW - this.pivot.x * w
+
+		if( this.align.h == "LEFT"){
+			x = x + this.pivot.x * w
+			this.anchor.x = x / pw
+		}else if(this.align.h == "CENTER"){
+			x = x - this.parentW/2 + pw/2 + this.pivot.x * w
+			this.anchor.x = x / pw
+		}else if(this.align.h == "RIGHT" ){
+			x = x - this.parentW + pw + this.pivot.x * w
+			this.anchor.x = x / pw
+		}else if(this.align.h == "STRETCH"){
+			let left = x
+			let right = x - this.parentW + pw + w
+			w = right - left
+			this.anchor.x = (left + this.pivot.x * w) / pw
+		}
+
+		this.size.x = w / pw
+		this.parentW = pw
+	}
+
+	ajustToParentwHeight(ph:number){
+		let h = this.size.y * this.parentH
+		let y = this.anchor.y * this.parentH - this.pivot.y * h   
+
+		if (this.align.v == "BOTTOM" ){
+			y = y + this.pivot.y * h
+			this.anchor.y = y / ph
+		}else if( this.align.v == "CENTER" ){
+			y = y - this.parentH/2 + ph/2 + this.pivot.y * h
+			this.anchor.y = y / ph
+		}else if( this.align.v == "TOP" ){
+			y = y - this.parentH + ph + this.pivot.y * h
+			this.anchor.y = y / ph
+		}else if( this.align.v == "STRETCH" ){
+			let left = y
+			let right = y - this.parentH + ph + h
+			h = right - left
+			this.anchor.y = (left + this.pivot.y * h) / ph
+		}
+
+		this.size.y = h / ph
+		this.parentH = ph
+	}
 
 	setFrame(frame:Frame){
 
@@ -197,29 +193,29 @@ export class Panel{
 
 	getFrame():Frame{
 		let pf = new Frame(0,0,love.window.getMode()[0],love.window.getMode()[1])
-	    if(this.parent){
-        	pf = this.parent.getFrame()
-        }
+		if(this.parent){
+			pf = this.parent.getFrame()
+		}
 
-        let w = this.size.x * pf.w
-        let h = this.size.y * pf.h
-        let x = this.anchor.x * pf.w - this.pivot.x * w
-        let y = this.anchor.y * pf.h - this.pivot.y * h
-        return new Frame(x,y,w,h)
-    }
+		let w = this.size.x * pf.w
+		let h = this.size.y * pf.h
+		let x = this.anchor.x * pf.w - this.pivot.x * w
+		let y = this.anchor.y * pf.h - this.pivot.y * h
+		return new Frame(x,y,w,h)
+	}
 
 
 	top(inside:boolean){
-    	let value:number = 0
-	    if(inside){
+		let value:number = 0
+		if(inside){
 			for(let i=0;i<this.children.length;i++){
 				let v = this.children[i]
 				value = math.max(value, this.tframe.y + v.frame.y + v.frame.h)
 			}
-	    }else{
-	        value = this.tframe.y + this.tframe.h 
-	    }
-	    return value
+		}else{
+			value = this.tframe.y + this.tframe.h 
+		}
+		return value
 	}
 
 	bottom(inside:boolean){
@@ -307,18 +303,10 @@ export class Panel{
 
 	hitTest(x:number,y:number):boolean{
 		return	x >= this.tframe.x && x <= this.tframe.x + this.tframe.w &&
-				y >= this.tframe.y && y <= this.tframe.y + this.tframe.h
+		y >= this.tframe.y && y <= this.tframe.y + this.tframe.h
 	}
 	touched(touch:Vec2):boolean{
-	    return this.interactive && this.visible && this.hitTest(touch.x, touch.y)
+		return this.interactive && this.visible && this.hitTest(touch.x, touch.y)
 	}
 
 }
-
-
-
-
-
-
-
-
