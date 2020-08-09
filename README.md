d0ba7cdf9b0354f5ccdd52baee4f6e7355e2730c
Mourey jerome <jerome@everycheck.fr>
2020-08-09 11:58:07 +0200

    intial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Ui
+
+Importing Ui package from Codea (iOS App to write lua app)
+
+## Setup
+- `npm install`
+- Have LOVE installed in path
+
+## Running
+- `npm run start`
+
+## Packing for sending 
+- `npm run pack`
+
---
+++ main.ts
@@ -0,0 +1,20 @@
+
+
+
+love.update = function(dt) {}
+
+love.draw = function() {
+}
+
+love.load = function() {
+
+}
+
+
+
+love.mousepressed = function( x:number, y:number, button:number,isTouch:boolean ){
+}
+
+love.mousereleased = function( x:number, y:number, button:number,isTouch:boolean ){
+
+}
