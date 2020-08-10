0f923a9d393d55a0a7b70e2184fb6f918762e448
Mourey jerome <jerome@everycheck.fr>
2020-08-10 09:49:30 +0200

    remove unnecessary module

--- conf.ts
+++ conf.ts
@@ -1,9 +1,27 @@
 love.conf = function(t) {
-  t.window.title = "Ui";
-  t.window.width = 800        
-  t.window.height = 600  
-};
+	t.window.title = "Ui";
+	t.window.width = 800
+	t.window.height = 600
 
+	t.modules.audio = false
+	t.modules.data = true
+	t.modules.event = true
+	t.modules.font = true
+	t.modules.graphics = true
+	t.modules.image = true
+	t.modules.joystick = false
+	t.modules.keyboard = true
+	t.modules.math = false
+	t.modules.mouse = true
+	t.modules.physics = false
+	t.modules.sound = false
+	t.modules.system = false
+	t.modules.thread = false
+	t.modules.timer = true
+	t.modules.touch = false
+	t.modules.video = false
+	t.modules.window = true
+};
 
 // function love.conf(t)
 //     t.identity = nil                    -- The name of the save directory (string)
